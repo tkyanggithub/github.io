a391da94f373759e752486fdc39d9b7827815302
tkyanggithub <123794394+tkyanggithub@users.noreply.github.com>
2024-07-22 13:45:48 +0800

    Add files via upload

---
+++ book_Fubon_01/mobile/javascript/bookmark_config.js
@@ -0,0 +1,1 @@
+﻿var ols=[{caption:"COVER-封面",page:"1",url:"",level:"0",children:[]},{caption:"INDEX-目錄",page:"3",url:"",level:"0",children:[]},{caption:"01-平面規劃",page:"4",url:"",level:"0",children:[]},{caption:"02-立面設計",page:"18",url:"",level:"0",children:[]}];
